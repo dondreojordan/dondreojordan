1fc306c8f78046951df736ff0c231adc87ae5b4b
dondreojordan <dondre-jordan@lambdastudents.com>
2021-02-04 01:13:15 -0800

    Testing, Attempt 3

--- portfolio_mainpg/SweetViz/TrainTest_Data Curation.py
+++ portfolio_mainpg/SweetViz/TrainTest_Data Curation.py
@@ -5,12 +5,12 @@ import time
 
 # Creating a report is a quick 3-line process:
 # Create a Pandas dataframe
-# upload = pd.read_csv('marketing_data.csv')
+upload = pd.read_csv('portfolio_mainpg\SweetViz\marketing_data.csv')
 # Create a copy of the DataFrame to work from
-# data = upload.copy()
+data = upload.copy()
 # Omit random state to have different random split each run
-# training_set = data.sample(frac=0.75, random_state=0)
-# testing_set = data.drop(training_set.index)
+training_set = data.sample(frac=0.75, random_state=0)
+testing_set = data.drop(training_set.index)
 
 # # (Optional) Visualize the Train/Split
 # print('Training set', train_set.shape)
@@ -24,7 +24,7 @@ import time
 # training_set.to_csv('Training_Set.csv')
 # testing_set.to_csv('Testing_Set.csv')
 
-def switchDataFrame(Training_set_csv:str, Testing_set_csv:str):
+def switchDataFrame(training_set, testing_set):
     """
     Invoke function to use time as an idicator for whether to launch HTML
     User Interface    SweetViz html page.
@@ -35,8 +35,7 @@ def switchDataFrame(Training_set_csv:str, Testing_set_csv:str):
     current_min = current_time[3:5]
     print(current_time)
         
-    training_set = pd.read_csv(Training_set_csv)
-    testing_set = pd.read_csv(Testing_set_csv)
+    
     if (int(current_min) % 2) == 0:  # Even minutes
         try:
             training_report = sv.analyze(training_set)
